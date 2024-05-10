aa737aee6d684314362e6bed0ff928f3396ce9c1
Sebastian Jimenez <jimenez1121monroy@gmail.com>
2024-05-09 22:35:42 -0500

    se agrego los metodos getter y setter de los objetos DTO

--- src/dto/AuhtDto.ts
+++ src/dto/AuhtDto.ts
@@ -1,18 +1,39 @@
 export default class Auth {
-    id ?: number
-    email : string;
-    password : string;
-    rol ?: string;
+    private _email : string;
+    private _password : string;
+    private _rol ?: string;
 
     constructor(
         email : string,
         password : string, 
-        id ?: number,
         rol ?: string,   
     ) {
-        this.email = email;
-        this.id = id
-        this.password = password;
-        this.rol = rol        
+        this._email = email;
+        this._password = password;
+        this._rol = rol        
+    }
+
+    get email():string{
+        return this._email
+    }
+
+    get password():string{
+        return this._password;
+    }
+
+    get rol():string | undefined{
+        return this._rol
+    }
+
+    set email(email: string){
+        this._email = email
+    }
+
+    set password(password: string){
+        this._password = password
+    }
+
+    set rol(rol: string){
+        this._rol = rol
     }
 }
--- src/dto/UserDto.ts
+++ src/dto/UserDto.ts
@@ -1,28 +1,86 @@
 class User{
-    email: string;
-    nombres: string;
-    apellidos: string;
-    edad: number;
-    rol: string
-    telefono: string;
-    password: string;
-    constructor(
-        email: string, 
-        nombres: string,
-        apellidos: string, 
-        telefono: string,
-        edad: number,
-        rol: string,
-        password: string
-    ) {
-        this.email = email;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.edad = edad;
-        this.rol = rol;
-        this.telefono = telefono;
-        this.password = password
-    }
+    private _email: string;
+    private _nombres: string;
+    private _apellidos: string;
+    private _edad: number;
+    private _rol: string;
+    private _telefono: string;
+    private _password: string;
+    
+        constructor(
+            email: string,
+            nombres: string,
+            apellidos: string,
+            telefono: string,
+            edad: number,
+            rol: string,
+            password: string
+        ) {
+            this._email = email;
+            this._nombres = nombres;
+            this._apellidos = apellidos;
+            this._edad = edad;
+            this._rol = rol;
+            this._telefono = telefono;
+            this._password = password; 
+        }
+    
+        get email(): string {
+            return this._email;
+        }
+    
+        get nombres(): string {
+            return this._nombres;
+        }
+    
+        get apellidos(): string {
+            return this._apellidos;
+        }
+
+        get edad(): number {
+            return this._edad;
+        }
+
+        get telefono(): string{
+            return this._telefono
+        }
+
+        get rol():string{
+            return this._rol
+        }
+
+        get password(): string {
+            return this._password
+        }
+    
+        set email(email : string) {
+            this._email = email
+        }
+    
+        set nombres(nombres: string) {
+            this._nombres = nombres;
+        }
+    
+        set apellidos(apellidos : string){
+            this._apellidos = apellidos
+        }
+
+        set edad(edad: number) {
+            this._edad = edad;
+        }
+    
+        set telefono(telefono: string) {
+            this._telefono = telefono;
+        }
+    
+        set rol(rol: string) {
+            this._rol = rol;
+        }
+
+        set password(password: string) {
+            this._password = password;
+        }
+    
 }
 
 export default User;
